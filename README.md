c4786beff81baefc284f0e66d72cda9843091551
carlosp420 <mycalesis@gmail.com>
2015-10-23 00:37:08 +0300

    script to fill authors

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-requires redis, and [anystyle-parser](https://github.com/inukshuk/anystyle-parser)
+## Requirements
+Requires redis, and [anystyle-parser](https://github.com/inukshuk/anystyle-parser)
+
+## Workflow
+* Use the MS Word document of the Butterfly Bibliography and convert to text of
+  1 column.
+* Convert the MS Word file to .txt format.
+* Keep only the biblio references in the .txt file by removing any uninformative
+  text. See image below:
+  
+![](https://rawgit.com/carlosp420/parsing-Lamas-references/master/media/example_of_input.png)
---
+++ fill_in_authors.py
@@ -0,0 +1,46 @@
+import argparse
+import re
+
+
+def clean_authors(authors):
+    """Remove dates of birth, death.
+    """
+    cleaned_authors = re.sub("\s*\[.+\]*", "", authors)
+    return cleaned_authors
+
+
+def fill_authors(input_file):
+    with open(input_file, "r") as handle:
+        data = handle.readlines()
+
+    new_data = []
+    authors = ""
+    for raw_line in data:
+        line = raw_line.rstrip().replace("\t", " ")
+        if not line.startswith("  "):
+            authors = clean_authors(line)
+        elif line.startswith(" ") and authors != "":
+            new_line = "{0}. {1}".format(authors, line)
+            new_line = new_line.replace("..", ".")
+            new_data.append(new_line)
+        else:
+            new_data.append(line)
+
+    with open("output.txt", "w") as handle:
+        handle.write("\n".join(new_data))
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Takes a txt version of Lamas' "
+                                                 "bibliography of butterflies and "
+                                                 "adds authors to each reference. ")
+    parser.add_argument('-i', '--input', dest='input_file', action='store',
+                        help='filename of txt file containing biblio references.')
+
+    args = parser.parse_args()
+    input_file = args.input_file
+    fill_authors(input_file)
+
+
+if __name__ == "__main__":
+    main()
